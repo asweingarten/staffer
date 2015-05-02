f1cf83fd6f3ec0b82706a563ed266339c87bf975
Ariel Weingarten <asweinga@uwaterloo.ca>
2015-05-02 15:56:34 -0400

    Add scale tests.

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,41 @@
 import {Note} from 'tests/lib/notes';
+import {Scale} from 'tests/lib/scales';
 var test = require('tape');
-test('timing test', function (t) {
-    t.plan(3);
-    var note = new Note(0);
-    t.deepEqual(note, new Note(0));
-    t.deepEqual(note.upHalfStep(), new Note(1));
-    t.deepEqual(note.downHalfStep(note), new Note(11));
+
+test('note stepping tests', function (t) {
+    t.plan(5);
+
+    var C_NATURAL = new Note(0);
+
+    t.deepEqual(C_NATURAL, new Note(0));
+
+    t.deepEqual(C_NATURAL.upHalfStep(), new Note(1));
+
+    t.deepEqual(C_NATURAL.upWholeStep(), new Note(2));
+
+    t.deepEqual(C_NATURAL.downHalfStep(), new Note(11));
+
+    t.deepEqual(C_NATURAL.downWholeStep(), new Note(10));
+});
+
+test('C Major scale construction', function(t) {
+	t.plan(8);
+
+	var C_NATURAL = new Note(0),
+		D_NATURAL = C_NATURAL.upWholeStep(),
+		E_NATURAL = D_NATURAL.upWholeStep(),
+		F_NATURAL = E_NATURAL.upHalfStep(),
+		G_NATURAL = F_NATURAL.upWholeStep(),
+		A_NATURAL = G_NATURAL.upWholeStep(),
+		B_NATURAL = A_NATURAL.upWholeStep(),
+		C_MAJOR_SCALE = new Scale(C_NATURAL, 'MAJOR');
+
+	t.deepEqual(C_MAJOR_SCALE.notes[0], C_NATURAL);
+	t.deepEqual(C_MAJOR_SCALE.notes[1], D_NATURAL);
+	t.deepEqual(C_MAJOR_SCALE.notes[2], E_NATURAL);
+	t.deepEqual(C_MAJOR_SCALE.notes[3], F_NATURAL);
+	t.deepEqual(C_MAJOR_SCALE.notes[4], G_NATURAL);
+	t.deepEqual(C_MAJOR_SCALE.notes[5], A_NATURAL);
+	t.deepEqual(C_MAJOR_SCALE.notes[6], B_NATURAL);
+	t.deepEqual(C_MAJOR_SCALE.notes[7], C_NATURAL);
 });
